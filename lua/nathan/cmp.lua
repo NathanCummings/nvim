cd6df715eeb1929dc1bd343c40cbb71cb4129980
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2022-04-19 10:29:50 +0100

    Update cmp

--- lua/nathan/cmp.lua
+++ lua/nathan/cmp.lua
@@ -119,11 +119,12 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	window = {
+		documentation = {
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		},
 	},
 	experimental = {
 		ghost_text = true,
-		native_menu = false,
 	},
 })
