bd5192b556964b2c4dc9bd937baa7d70e4d9f065
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2023-10-27 18:52:29 +0100

    Add descriptions for mapped keys

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,23 +21,20 @@ local opts = {
 		lazy = true,
 	},
 	install = {
-		colorscheme = { "gruvbox-material" }
+		colorscheme = { "gruvbox-material" },
 	},
 	rtp = {
 		disabled_plugins = {
 			"gzip",
 			"matchit",
-                        "matchparent",
-                        "netrwPlugin",
-                        "tarPlugin",
-                        "tohtml",
-                        "tutor",
-                        "zipPlugin",
-                 }
-	 },
-	 change_detection = {
-		 notify = true,
-	 },
- }
+			"matchparent",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+}
 
-require("lazy").setup('plugins', opts)
+require("lazy").setup("plugins", opts)
--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -2,23 +2,28 @@ local mapkey = require("util.keymapper").mapkey
 
 local M = {}
 
+local add_desctiption = function(options, description)
+  options.desc = description
+  return options
+end
+
 -- set keymaps on the active lsp server
 M.on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	mapkey("<leader>fd", "Lspsaga finder", "n", opts) -- go to definition
-	mapkey("<leader>gd", "Lspsaga peek_definition", "n", opts) -- peak definition
-	mapkey("<leader>gD", "Lspsaga goto_definition", "n", opts) -- go to definition
-	mapkey("<leader>ca", "Lspsaga code_action", "n", opts) -- see available code actions
-	mapkey("<leader>rn", "Lspsaga rename", "n", opts) -- smart rename
-	mapkey("<leader>D", "Lspsaga show_line_diagnostics", "n", opts) -- show  diagnostics for line
-	mapkey("<leader>d", "Lspsaga show_cursor_diagnostics", "n", opts) -- show diagnostics for cursor
-	mapkey("<leader>pd", "Lspsaga diagnostic_jump_prev", "n", opts) -- jump to prev diagnostic in buffer
-	mapkey("<leader>nd", "Lspsaga diagnostic_jump_next", "n", opts) -- jump to next diagnostic in buffer
-	mapkey("K", "Lspsaga hover_doc", "n", opts) -- show documentation for what is under cursor
+	mapkey("<leader>fd", "Lspsaga finder", "n", add_desctiption(opts, "Show references"))
+  mapkey("<leader>gd", "Lspsaga peek_definition", "n", add_desctiption(opts, "Peek definition"))
+	mapkey("<leader>gD", "Lspsaga goto_definition", "n", add_desctiption(opts, "Go to definition"))
+	mapkey("<leader>ca", "Lspsaga code_action", "n", add_desctiption(opts, "see available code actions"))
+	mapkey("<leader>rn", "Lspsaga rename", "n", add_desctiption(opts, "Rename"))
+	mapkey("<leader>D", "Lspsaga show_line_diagnostics", "n", add_desctiption(opts, "Line diagnostics"))
+	mapkey("<leader>d", "Lspsaga show_cursor_diagnostics", "n", add_desctiption(opts, "Cursor diagnostics"))
+	mapkey("<leader>pd", "Lspsaga diagnostic_jump_prev", "n", add_desctiption(opts, "Previous diagnostic"))
+	mapkey("<leader>nd", "Lspsaga diagnostic_jump_next", "n", add_desctiption(opts, "Next diagnostic"))
+	mapkey("K", "Lspsaga hover_doc", "n", add_desctiption(opts, "Documentation for symbol under cursor"))
 
 	if client.name == "pyright" then
-		mapkey("<Leader>oi", "PyrightOrganizeImports", "n", opts)
+		mapkey("<Leader>oi", "PyrightOrganizeImports", "n", add_desctiption(opts, "Organise imports"))
 	end
 end
 
