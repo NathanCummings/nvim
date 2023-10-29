bb8a7c89ab5c4d56f83f3eb665071a23f00489ad
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2023-10-29 12:26:11 +0000

    Add nord

--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -1,9 +1,8 @@
 return {
-  'sainnhe/gruvbox-material',
-	lazy = false,
-	priority = 999,
-  config = function()
-    vim.cmd('colorscheme gruvbox-material')
-    end
+	"sainnhe/gruvbox-material",
+	-- lazy = false,
+	-- priority = 999,
+	-- config = function()
+	-- 	vim.cmd("colorscheme gruvbox-material")
+	-- end,
 }
- 
