b3fcf667d0bb116c11bee895b40e74408d3c21b0
Nathan Cummings <NathanCummings@users.noreply.github.com>
2023-11-07 15:18:25 +0000

    Update toggleterm.lua
    Support python REPL

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,19 @@
 return {
 	"akinsho/toggleterm.nvim",
-  lazy = false,
+        lazy = false,
 	version = "*",
 	opts = {
 		open_mapping = [[<C-t>]],
-    shade_terminals = false,
-    direction = "float"
+                shade_terminals = false,
+                direction = "float",
+		float_opts = { border = "curved" },
 	},
+	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local python = Terminal:new({ cmd = "python", hidden = true })
+
+		function _PYTHON_TOGGLE()
+			python:toggle()
+		end
+	end
 }
