02ec6c7e388bf1f8be93f902e6a97ff87d7ca786
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2023-12-27 14:52:45 +0000

    Fix format on save.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,5 @@
 local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-vim.api.nvim_create_autocmd('BufWritePost', {
+vim.api.nvim_create_autocmd('BufWritePre', {
   group = lsp_fmt_group,
   callback = function(ev)
     local efm = vim.lsp.get_active_clients({ name = 'efm', bufnr = ev.buf })
