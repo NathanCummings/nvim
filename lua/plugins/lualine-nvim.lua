e2fa8573ee10f28f4f922b7cabf799e9c469d92d
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2024-03-24 19:56:49 +0000

    Update lualine.

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,25 +1,27 @@
 local config = function()
-	local custom_nord = require("lualine.themes.nord")
-	custom_nord.normal.c.bg = "None"
+    local custom_nord = require("lualine.themes.nord")
+    custom_nord.normal.c.bg = "None"
 
-	require("lualine").setup({
-		options = {
-			theme = custom_nord,
-			globalstatus = true,
-			disabled_filetypes = { "alpha" },
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "buffers" },
-			lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-	})
+    require("lualine").setup({
+        options = {
+            theme = custom_nord,
+            globalstatus = true,
+            disabled_filetypes = { "alpha" },
+            component_separators = '|',
+            section_separators = { left = '', right = '' },
+        },
+        sections = {
+            lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+            lualine_b = { "buffers" },
+            lualine_x = { "encoding", { "fileformat", symbols = { unix = " " } }, "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+        },
+    })
 end
 
 return {
-	"nvim-lualine/lualine.nvim",
-	lazy = false,
-	config = config,
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    config = config,
 }
