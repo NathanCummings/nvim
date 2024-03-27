bdfa840fb782ac0de2e4e9b73be2c9a46b32e544
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2024-03-27 21:56:24 +0000

    Activate evrforest theme

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,9 +1,9 @@
 return {
 	"neanias/everforest-nvim",
-	-- lazy = false,
-	-- priority = 999,
-	-- config = function()
+	lazy = false,
+	priority = 999,
+	config = function()
 	-- 	vim.cmd("colorscheme everforest")
-	-- 	vim.g.nord_borders = true
-	-- end,
+		vim.g.nord_borders = true
+	end,
 }
