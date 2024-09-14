3fbee4e367e260734c1ada955d2f857f80b87d9e
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2024-09-14 16:05:52 +0100

    Fix border, update ref to ts_lsp

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,9 +1,10 @@
 return {
-	"neanias/everforest-nvim",
-	lazy = false,
-	priority = 999,
-	config = function()
-		vim.cmd("colorscheme everforest")
-		vim.g.nord_borders = true
-	end,
+    "neanias/everforest-nvim",
+    lazy = false,
+    priority = 999,
+    config = function()
+        vim.cmd("colorscheme everforest")
+        vim.g.nord_borders = true
+        vim.cmd("highlight FloatBorder guibg=none")
+    end,
 }
--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -2,5 +2,19 @@ return {
     "echasnovski/mini.files",
     version = false,
     lazy = false,
-    opts = { mappings = { go_in_plus = '<CR>', synchronize = 's' }, windows = { preview = true, width_preview = 80 } },
+    -- opts = { mappings = { go_in_plus = '<CR>', synchronize = 's' }, windows = { preview = true, width_preview = 80 } },
+    config = function()
+        require("mini.files").setup { mappings = { go_in_plus = '<CR>', synchronize = 's' }, windows = { preview = true, width_preview = 80 } }
+        -- Add rounded corners
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'MiniFilesWindowOpen',
+            callback = function(args)
+                local win_id = args.data.win_id
+                vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
+            end,
+        })
+        vim.cmd("highlight MiniFilesTitle guibg=#00ffffff")
+        vim.cmd("highlight MiniFilesTitleFocused guibg=#00ffffff")
+        vim.cmd("highlight MiniFilesBorder none")
+    end
 }
