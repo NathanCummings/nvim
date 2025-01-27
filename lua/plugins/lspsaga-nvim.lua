e42fa2bf936f6505143b57d256c57fe84b837597
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2025-01-27 10:12:40 +0000

    Formatting

--- lua/plugins/lspsaga-nvim.lua
+++ lua/plugins/lspsaga-nvim.lua
@@ -1,27 +1,27 @@
 return {
-	"glepnir/lspsaga.nvim",
-	lazy = false,
-	config = function()
-		require("lspsaga").setup({
-			code_action = {
-				extend_gitsigns = true,
-			},
-			-- keybinds for navigation in lspsaga window
-			move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-			-- use enter to open file with finder
-			finder_action_keys = {
-				open = "<CR>",
-			},
-			-- use enter to open file with definition preview
-			definition_action_keys = {
-				edit = "<CR>",
-			},
+    "glepnir/lspsaga.nvim",
+    lazy = false,
+    config = function()
+        require("lspsaga").setup({
+            code_action = {
+                extend_gitsigns = true,
+            },
+            -- keybinds for navigation in lspsaga window
+            move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+            -- use enter to open file with finder
+            finder_action_keys = {
+                open = "<CR>",
+            },
+            -- use enter to open file with definition preview
+            definition_action_keys = {
+                edit = "<CR>",
+            },
             lightbulb = {
                 enable = false,
             }
-		})
-	end,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
+        })
+    end,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
 }
