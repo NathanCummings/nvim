d5fe71e699728867f1667c220780ebcbdb1c360a
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2025-01-27 10:59:15 +0000

    Add auto-save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,13 @@ vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
     pattern = "*",
     callback = function(args)
         local buf = args.buf or vim.api.nvim_get_current_buf()
+
+        -- Exclude mini.files buffers or any other specific filetypes
+        local filetype = vim.bo[buf].filetype
+        if filetype == "minifiles" then
+            return
+        end
+
         if vim.fn.mode() == "n" then
             vim.defer_fn(function()
                 if vim.api.nvim_buf_is_valid(buf) then
